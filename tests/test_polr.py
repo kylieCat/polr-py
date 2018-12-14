00e809e92c3572d26e2b11af651809069116736e
Ian Auld <imauld@gmail.com>
2018-12-13 18:13:21 -0800

    Initial

---
+++ tests/test_polr.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `polr` package."""
+
+
+import unittest
+from click.testing import CliRunner
+
+from polr import polr
+from polr import cli
+
+
+class TestPolr(unittest.TestCase):
+    def test_command_line_interface(self):
+        """Test the CLI."""
+        runner = CliRunner()
+        result = runner.invoke(cli.polr)
+        assert result.exit_code == 0
+        assert 'polr.cli.polr' in result.output
+        help_result = runner.invoke(cli.polr, ['--help'])
+        assert help_result.exit_code == 0
+        assert '--help  Show this message and exit.' in help_result.output
