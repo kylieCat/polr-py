8c3237c364f35b9d31e6bd96e9094afbbb933d7e
Ian Auld <imauld@gmail.com>
2018-12-19 10:47:08 -0800

    give up on env vars

--- polr/settings.py
+++ polr/settings.py
@@ -6,8 +6,8 @@ from .utils import print_error
 
 # Required settings
 try:
-    POLR_API_KEY = os.environ["POLR_API_KEY"]
-    POLR_URL = os.environ["POLR_URL"]
+    POLR_API_KEY = os.environ.get("POLR_API_KEY", "1234")
+    POLR_URL = os.environ.get("POLR_URL", "http://polr.example.com")
 except KeyError as err:
     print_error(f"missing required env var: {err}")
     sys.exit(1)
